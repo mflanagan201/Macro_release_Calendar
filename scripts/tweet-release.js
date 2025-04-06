ff06d66f514ef4e3af280d15e3b040696c71f387
mflanagan201 <125045921+mflanagan201@users.noreply.github.com>
2025-04-06 21:40:46 +0200

    Update tweet-release.js

--- scripts/tweet-release.js
+++ scripts/tweet-release.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const client = new TwitterApi({
   appKey: process.env.TWITTER_API_KEY,
   appSecret: process.env.TWITTER_API_SECRET,
-  accessToken: process.env.TWITTER_ACCESS_CODE,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
   accessSecret: process.env.TWITTER_ACCESS_SECRET,
 });
 
